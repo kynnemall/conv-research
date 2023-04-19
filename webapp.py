5de8b9da02f367b34d1947ef461b8bce632ebe04
martin <sideproject1892@gmail.com>
2023-04-19 18:41:24 +0100

    Make index page and prepare requirements

---
+++ webapp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 19 18:17:36 2023
+
+@author: martin
+"""
+
+import streamlit as st
+
+st.markdown(
+'''
+This app serves as a digital notebook to track experiments related to
+convolutional neural networks. Each page documents an experiment
+
+* Separable Conv: The ability to reproduce a 2D kernel from combinations of 1D 
+kernels
+''',
+unsafe_allow_html=True
+)
