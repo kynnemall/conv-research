ebf7ea757ffff841606887c11b8df267a690a993
martin <sideproject1892@gmail.com>
2023-04-20 23:06:10 +0100

    Finish page and allow model training

--- pages/Separable Conv.py
+++ pages/Separable Conv.py
@@ -7,28 +7,21 @@ Created on Wed Apr 19 18:17:36 2023
 """
 
 import numpy as np
-import pandas as pd
 import streamlit as st
-import plotly.express as px
 import plotly.graph_objects as go
 from skimage import data
-from tensorflow.keras import layers, models, optimizers, callbacks, Sequential
+from scipy import signal
+from tensorflow.keras import layers, models, optimizers, callbacks
 
 
-@st.cache_data
-def load_image():
-    """
-    Load the 'cat' image from scikit-image
-
-    Returns
-    -------
-    image : 2D numpy array
-        Sample image.
-
-    """
-    image = data.load('cat')
-    image = np.expand_dims(image, axis=0)
-    return image
+def my_conv2d(image, kernel):
+    # Apply the convolution to each channel separately
+    result = np.zeros_like(image)
+    for i in range(image.shape[-1]):  # Loop over the channels
+        result[:, :, i] = signal.convolve2d(
+            image[:, :, i], kernel, mode='same'
+        )
+    return result
 
 
 def gaussian_kernel(l=5, sigma=1.):
@@ -92,31 +85,6 @@ def generate_kernel(n_kernels, filter_size):
 
 
 @st.cache_resource
-def whole_conv_model(filter_size):
-    """
-    Initialize a model with 1 convolutional layer containing 1 filter
-
-    Parameters
-    ----------
-    filter_size : integer
-        Size of the filter in the model.
-
-    Returns
-    -------
-    model : Keras model
-        Single-layer Conv2D model
-
-    """
-    model = Sequential()
-    model.add(
-        layers.Conv2D(
-            1, filter_size, activation='tanh', padding='same'
-        )
-    )
-    return model
-
-
-@st.cache_resource
 def sep_conv_model(filter_size, h_filters, v_filters):
     """
     Initialize a model with 1 layer of separate convolutions for height and 
@@ -138,38 +106,55 @@ def sep_conv_model(filter_size, h_filters, v_filters):
         Model with separate 1D convolutions
 
     """
-    in_ = layers.Input(shape=())
-    h = layers.Conv2D(h_filters, (1, filter_size), activation='tanh',
+    in_ = layers.Input(shape=(300, 451, 3))
+    h = layers.Conv2D(h_filters, (1, filter_size), activation='sigmoid',
                       padding='same')(in_)
-    v = layers.Conv2D(v_filters, (filter_size, 1), activation='tanh',
+    v = layers.Conv2D(v_filters, (filter_size, 1), activation='sigmoid',
                       padding='same')(in_)
     out = layers.Add()([h, v])
     model = models.Model(inputs=in_, outputs=out)
     return model
 
 
-def train_model(model, training_data):
+def train_model(model, kernel, label, epochs=300):
+    # prep training data
+    image = data.cat().astype(np.float32)
+    image = image / 255.
+    conv_image = my_conv2d(image, kernel)
+    conv_image = np.expand_dims(conv_image, axis=0)
+    image = np.expand_dims(image, axis=0)
+
     # set up training loop
     reducelr = callbacks.ReduceLROnPlateau(
-        'loss', patience=10, min_delta=0.001
+        'loss', patience=20, min_delta=0.05
     )
-    model.compile(optimizers.Adam(lr=0.003), loss='mse')
+    model.compile(optimizers.Adam(lr=0.003), loss='mae')
 
-    history = model.fit(training_data, epochs=200, callbacks=[reducelr])
+    history = model.fit(
+        image, conv_image, epochs=epochs, batch_size=1,
+        callbacks=[reducelr]
+    )
 
     # plot loss
-    hist = pd.DataFrame(history)
-    fig = px.line(data=hist, x='Epochs', y='Loss')
+    fig = go.Figure(
+        go.Scatter(
+            x=np.arange(1, epochs + 1), y=history.history['loss'], name=label
+        )
+    )
 
-    return fig
+    model_result = np.squeeze(model(image))
+    model_result = model_result / model_result.max(axis=(0, 1))
+    conv_image = np.squeeze(conv_image)
+    conv_image = conv_image / conv_image.max(axis=(0, 1))
+
+    return fig, conv_image, model_result
+
 
 # %%
-
-
-image = load_image()
+fig = False
 
 with st.form('Experiment settings'):
-    size = st.slider('Kernel size', value=3, max_value=31, min_value=3, step=2)
+    size = st.slider('Kernel size', value=9, max_value=31, min_value=7, step=2)
     n_kernels = st.slider('Number of Gaussian filters to use in the kernel',
                           value=3, max_value=80, min_value=3, step=1)
     h_filters = st.slider('Number of horizontal filters', value=1,
@@ -182,8 +167,13 @@ with st.form('Experiment settings'):
     submitted = st.form_submit_button("Submit")
     if submitted:
         kernel = generate_kernel(n_kernels, size)
-        whole_model = whole_conv_model(size)
         sep_model = sep_conv_model(size, h_filters, v_filters)
 
-        train_model(whole_model, image)
-        train_model(sep_model, image)
+        fig, convimg, modimg = train_model(
+            sep_model, kernel, 'Multiple sub-kernels'
+        )
+
+if fig:
+    st.plotly_chart(fig)
+    st.image(convimg, caption='Convolved image')
+    st.image(modimg, caption='Model result')
